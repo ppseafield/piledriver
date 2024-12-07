8fb8e481db5477e92058628d0548759391600883
Patrick P. Seafield <patrick@ppseafield.com>
2024-12-06 23:40:43 -0800

    Added base frontend, nuxt 4 compat.

---
+++ piledriver-frontend/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-12-06",
+  future: {
+    compatibilityVersion: 4
+  },
+
+  typescript: {
+    typeCheck: true
+  },
+  devtools: { enabled: true },
+
+  extends: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui"]
+})
