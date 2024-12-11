98751a72bf804c37bc360394cba2637467ffccfe
Patrick P. Seafield <patrick@ppseafield.com>
2024-12-11 12:13:11 -0800

    Change generate_ulid to reference piledriver schema.

--- piledriver-flyway/migrations/V009_20241210032428__amend generate_ulid function.sql
+++ piledriver-flyway/migrations/V009_20241210032428__amend generate_ulid function.sql
@@ -1,6 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE OR REPLACE FUNCTION piledriver.generate_ulid() RETURNS uuid
     AS $$
         SELECT (lpad(to_hex(floor(extract(epoch FROM clock_timestamp()) * 1000)::bigint), 12, '0') || encode(piledriver.gen_random_bytes(10), 'hex'))::uuid;
