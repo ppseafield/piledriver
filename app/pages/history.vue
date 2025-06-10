8c561d145648ca82a1bafe1693d6929e9192c9e0
Patrick P. Seafield <patrick@ppseafield.com>
2025-06-09 22:24:09 -0700

    Idea: stream of all tasks by date.

---
+++ app/pages/history.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineI18nRoute({
+  paths: {
+    en: '/history',
+    de: '/chronik'
+  }
+})
+
+// TODO: paginated table of all tasks
+</script>
+
+<template>
+  <div>history WIP</div>
+</template>
