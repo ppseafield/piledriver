f8f91155c1d7b6d090d354d7df023947f5322bcb
Patrick P. Seafield <patrick@ppseafield.com>
2025-08-25 23:16:40 -0700

    Remove console.log from middleware

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const session = useUserSession()
   const localePath = useLocalePath()
-  console.log('auth middleware - session:', session)
+
   if (!session.user.value) {
     return navigateTo(localePath('/login'))
   }
