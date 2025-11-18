aa55728911e11f4997262809dce69dfa7cb7e5c5
Patrick P. Seafield <patrick@ppseafield.com>
2025-11-17 17:45:04 -0800

    Bend over backwards to get a single nuxt component test to run.

--- test/nuxt/components/ProfileActions.spec.ts
+++ test/nuxt/components/ProfileActions.spec.ts
@@ -1,9 +1,33 @@
 import { describe, test, expect } from 'vitest'
-// import { mountSuspended } from '@nuxt/test-utils/runtime'
-// import { ProfileActions } from '#components'
+import { mount } from '@vue/test-utils'
+import { createI18n } from 'vue-i18n'
+import { RouterLink } from 'vue-router'
+import { ProfileActions, NuxtLink } from '#components'
+import en from '@@/i18n/locales/en.json'
+import de from '@@/i18n/locales/en.json'
+
 
 test('Profile actions empty when logged out', async () => {
-  // const pa = await mountSuspended(ProfileActions)
-  // expect(pa.text()).toContain('lmao')
+  // This is the workaround.... EVERY TIME?!
+  const i18n = createI18n({
+    legacy: false,
+    globalInjection: true,
+    locale: 'en',
+    messages: { en, de }
+  })
+
+  const pa = await mount(
+    ProfileActions,
+    {
+      global: {
+	plugins: [i18n],
+	components: { NuxtLink, RouterLink },
+	provide: {
+	  [Symbol.for('nuxt-ui.locale-context')]: i18n
+	}
+      }
+    }
+  )
+  expect(pa.text()).toContain('Account')
   expect('todo: fix i18n to work in components for testing').toBeTruthy()
 })
--- vitest.global-setup.ts
+++ vitest.global-setup.ts
@@ -1,17 +1,5 @@
-// import { vi } from 'vitest'
-// import { mockNuxtImport } from '@nuxt/test-utils/runtime'
-
 export const setup = () => {
   process.env.TZ = 'America/Los_Angeles'
   process.env.LC_ALL = 'en-us.UTF-8'
-
-  // TODO: get nuxt i18n working with mountSuspended from @nuxt/test-utils.
-  // mockNuxtImport("useI18n", async () => {
-  //   const actualNuxtAppImports =
-  //     await vi.importActual<typeof import("#app")>("#app");
-
-  //   return () => {
-  //     return actualNuxtAppImports.useNuxtApp().$i18n;
-  //   };
-  // });
 }
+
